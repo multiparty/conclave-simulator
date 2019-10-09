8b963f1998d250457ab81f66e76d44f6316d6cb0
Wyatt Howe <wyatt@wyatthowe.net>
2019-10-09 17:47:20 -0400

    finish adapting carousels' interface

--- web/index.js
+++ web/index.js
@@ -3,24 +3,29 @@ const utils = simulator.utils;
 const ver = simulator.Verifier;
 
 function demo() {
-  var workflow = new Function($('#workflow').text());
+  try {
+    let workflow = new Function($('#workflow').val());
 
-  let policyOne = JSON.parse($('#policyOne').text());
-  let vOne = new ver.Verify(workflow(), policyOne);
-  let resOne = vOne.verify();
+    let policyOne = JSON.parse($('#policyOne').val());
+    let vOne = new ver.Verify(workflow(), policyOne);
+    let resOne = vOne.verify();
 
-  let policyTwo = JSON.parse($('#policyTwo').text());
-  let vTwo = new ver.Verify(workflow(), policyTwo);
-  let resTwo = vTwo.verify();
+    let policyTwo = JSON.parse($('#policyTwo').val());
+    let vTwo = new ver.Verify(workflow(), policyTwo);
+    let resTwo = vTwo.verify();
 
-  var output = '';
-  if (resOne === true && resTwo === false) {
-    output = 'Verification Passed.';
-  } else {
-    output = 'Verification Failed.';
+    if (resOne === true && resTwo === false) {
+      output = 'Verification Passed.';
+    } else {
+      output = 'Verification Failed.';
+    }
+    $('#out').text(output);// + '\n\n' + workflow());
+
+    console.log(output, workflow());
+    return [resOne, resTwo, output, policyOne, policyTwo, workflow, workflow()];
+  } catch (e) {
+    $('#out').text(e);
+    throw e;
+    return 'Syntax Error: ' + e;
   }
-  $('#out').text(output);// + '\n\n' + workflow());
-
-  console.log(output, workflow());
-  return [resOne, resTwo, output, policyOne, policyTwo, workflow, workflow()];
 }
